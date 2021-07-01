12fe910eaab0f0ffea9dce9dd5d2d5103826a429
aliabbas666 <50536269+aliabbas666@users.noreply.github.com>
2021-07-02 00:22:46 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,441 @@
+// **********CHAPTER 1 *************
+
+//1. Write a script to greet your website visitor using JS alert box.
+
+// alert("Welcome to our platform")
+
+// 2. Write a script to display following message on your web page:
+
+// alert("Error! Please enter a valid password.")
+
+// 3. Write a script to display following message on your web page: (Hint : Use line break)
+
+// alert("Welcome to JS Land... \n Happy Coding!")
+
+// 4. Write a script to display following messages in sequence:
+
+// alert("Welcome to JS Land...")
+// alert("Happy Coding!")
+
+// 5. Generate the following message through browser’s developer console:
+
+// console.log("Hello... I can run JS through my web browser's console")
+
+// 6. Make use of alerts in your new/existing HTML & CSS project.
+// OK
+
+// 7. Practice placement of <script></script> element in
+// following sections of your project in exercise 6:
+// a. Head
+// b. Body (before your page’s HTML)
+// c. Body (inside your page’s HTML)
+// d. Body (after your page’s HTML)
+
+//*****CHAPTER 2 */
+// 1. Declare a variable called username.
+
+// var username;
+
+//2. Declare a variable called myName & assign to it a string that represents your Full Name.
+// var myName="Ali Abbas";
+
+// 3. Write script to
+// a) Declare a JS variable, titled message.
+// b) Assign “Hello World” to variable message
+// c) Display the message in alert box.
+
+// var message="Hello world"
+// alert(message)
+
+// 4. Write a script to save student’s bio data in JS variables and
+// show the data in alert boxes.
+// var name="ALi"
+// var age=21
+// var job="DevOps engineer"
+// alert(name)
+// alert(age)
+// alert(job)
+
+// 5. Write a script to display the following alert using one JS
+// variable:
+
+// var food="PIZZA"
+// alert(food+"\n"+food.slice(0,4)+"\n"+food.slice(0,3)+"\n"+food.slice(0,2)+"\n"+food.slice(0,1))
+
+// 6. Declare a variable called email and assign to it a string that
+// represents your Email Address(e.g. example@example.com).
+// Show the blow mentioned message in an alert box.(Hint: use
+// string concatenation)
+
+// var email="example@example.com"
+// alert("My email address is" + email)
+
+// 7. Declare a variable called book & give it the value “A
+// smarter way to learn JavaScript”. Display the following
+// message in an alert box:
+
+// var book="A smarter way to learn JavaScript"
+// alert(book)
+
+
+// 8. Write a script to display this in browser through JS
+
+// document.write("Yah! I can write  HTML content through Javascript")
+
+// 9. Store following string in a variable and show in alert and
+// browser through JS
+
+// var temp="“ ▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬ ”"
+// alert(temp)
+
+
+//***** CHAPTER #3 */
+//TASK1
+// var age=22
+// alert("I am "+age+" years old")
+//TASK2
+
+// var no=10
+// alert("You have visited this site "+no+" times")
+
+//TASK3
+// //var birhyear=1997
+// document.write("My birth year is"+birthyear)
+// document.write("Data of my declared variable is number")
+
+// Task4
+// var visitor_name="Ali"
+// var product_title="shoes"
+// var Quantity=3
+
+// document.write(visitor_name+" ordered "+Quantity+" "+product_title+" On XYZ Clothing Store")
+
+//************ CHAPTER 4 **************//
+
+//TASK 1
+// var name1,name2,name3;
+
+// TASK2
+//>>>>>> Legal variables name
+// var name,first_name,LastName,MIDDLENAME,FULL_name1212;
+//>>>>>> Illegal variables name
+// var 1name,$first_name,Last%name,MIDDLENA))ME,FUL@L_name;
+
+//TASK3
+// a) 
+// document.write("<h1>Rules for naming JS variables<h1>")
+// b)
+//lowercase letter,uppercase letter,underscore and numbers but not in the beginging
+// c)
+//_name and name
+// d)
+// casesensitive
+// e)
+// should not be reserved words
+
+
+
+////////////////////////CHAPTER 5 ///////////////////////
+
+// //TASK1
+// var no1=prompt("Enter first number")
+// var no2=prompt("Enter second number")
+// document.write("Sum of "+no1 +" and" + no2+ " is "+ no1+no2)
+
+// //TASK2
+// var no1=prompt("Enter first number")
+// var no2=prompt("Enter second number")
+// document.write("Subtraction of "+no1 +" and" + no2+ " is "+ no1-no2)
+
+// var no1=prompt("Enter first number")
+// var no2=prompt("Enter second number")
+// document.write("Division of "+no1 +" and" + no2+ " is "+ no1/no2)
+
+// var no1=prompt("Enter first number")
+// var no2=prompt("Enter second number")
+// document.write("Multiplcation of "+no1 +" and" + no2+ " is "+ no1-no2)
+
+//TASK3
+// var temp;
+// document.write("Value after variable declaration is: "+temp)
+// temp=5;
+// document.write("Initial value: "+temp)
+// temp++;
+// document.write("Value after increment is: "+temp)
+// temp+=7;
+// document.write("Value after addition is: "+temp)
+// temp--;
+// document.write("Value after decrement is: "+temp)
+// temp%=3;
+// document.write("The remainder is : "+temp)
+
+//TASK4
+// var cost=600;
+// var no=5
+// document.write("Total cost to buy "+no+" tickets to a movie is "+cost*no+"PKR")
+
+//TASK5
+// var no=prompt("Enter number for table is :")
+// for(let i=1;i<=10;i++)
+// document.write(no+" x "+i+" = "no*i)
+
+//TASK6
+// var tempc=50 //in celsius
+// var tempf;
+// tempf=(tempc*9/5)+32
+// document.write(tempc+"C is"+tempf+"F")
+// var tempf=50 //in celsius
+// var tempc;
+// tempc=(tempf-32)*5/9
+// document.write(tempf+"F is"+tempc+"C")
+
+// Task7
+// var item1_p=650
+// var item1_Q=3
+// var item2_p=100
+// var item2_q=7
+// var shipping_charges=100
+// document.write("Total cost of your order is " item1_Q*item1_p+item2_p*item2_q+shipping_charges)
+
+//TASK8
+// var total_marks=980
+// var obtain_marks=804
+// document.write("Percentage: "+ (obtain_marks/total_marks*100)+"%")
+
+//TASK9
+// var t_dollars=10
+// var t_riyal=25
+// var ustopkr=104.80
+// var riyaltopkr=28
+// document.write("Total Currency in PKR: "+t_dollars*ustopkr+t_riyal*riyaltopkr)
+
+//TASK10
+// var temp=5
+// temp=((temp+5)*10)/2
+
+//TASK11
+// var current_year=2021
+// var birth_year=1997
+// document.write("Your age is either"+current_year-birth_year+" or "+current_year-birth_year+1)
+
+// TASK12
+// var radius=25
+// document.write("The circumference is "+2*3.14*radius)
+// document.write("The area is "+3.14*radius*radius)
+
+// //TASK13
+// var favorite_snack="Snacks"
+// var current_age=15
+// var estimated_age=65
+// var amount_per_day=3
+// document.write("You will need "+(estimated_age-current_age)*3+favorite_snack+" to last you until the ripe old age of 65")
+
+
+//*******CHAPTER6 to 9 ****************/
+
+//TASK1
+
+// var a=prompt("Enter a number ")
+// document.write("Result \n")
+// document.write("The value of a is : "+a)
+// document.write(".......................")
+// document.write("The value of ++a is : "+(++a))
+// document.write("Now the value of a is : "+a)
+// document.write("The value of a++ is : "+(a++))
+// document.write("Now the value of a is : "+a)
+// document.write("The value of --a is : "+(--a))
+// document.write("Now the value of a is : "+a)
+// document.write("The value of a-- is : "+(a--))
+// document.write("Now the value of a is : "+a)
+
+//TASK2
+
+// 2. What will be the output in variables a, b & result after
+// execution of the following script:
+// var a = 2, b = 1;
+// var result = --a - --b + ++b + b--;
+// Explain the output at each stage:
+// --a; a=1
+// --a - --b; a=1,b=0.result=1
+// --a - --b + ++b;a=1,b=1,result=2
+// --a - --b + ++b + b--;a=1,b=0,result=3
+// document.write("a is "+a+"\n"+" b is "+b+" \n "+" Result is "+result)
+
+//TASK3
+// var name=prompt("What is your name")
+// alert("Welcome "+name)
+
+//TASK4
+//there is no task in the assignment for this question
+
+//Task5
+// var no=prompt("Enter the number for the table")
+// if(no !="")
+// for(let i=1;i<=10;i++)
+// document.write(no+" x "+i+" = "no*i)
+// else
+// for(let i=1;i<=10;i++)
+// document.write("5"+" x "+i+" = "5*i)
+
+//TASK6
+// var sub1=prompt("Enter subject name for 1")
+// var sub2=prompt("Enter subject name for 2")
+// var sub3=prompt("Enter subject name for 3")
+// var total=100
+// var obt1=prompt("Enter the obtained marks for the first subject")
+// var obt2=prompt("Enter the obtained marks for the 2nd subject")
+// var obt3=prompt("Enter the obtained marks for the 3rd subject")
+// document.write("<br>"+"Obtain percentage for subj1:"+obt1/total*100+"%")
+// document.write("<br>"+"Obtain percentage for subj2:"+obt2/total*100+"%")
+// document.write("<br>"+"Obtain percentage for subj3:"+obt3/total*100+"%")
+// document.write("<br>"+"Total Marks is "+total*3+" Obtain marks is "+obt1+obt2+obt3+"Total percentage is"+(obt1+obt2+obt3)/(total*3*100)+"%")
+
+
+
+//***************** CHAPTER9 to 11 */
+
+//TASK1
+// // var city=prompt("Enter name of the city")
+// if(city=="Karachi")
+// alert("Welcome to the city of lights")
+
+//TASK2
+// var gender=prompt("Enter your gender")
+// if(gender=="male")
+// alert("Good Morning Sir")
+// else
+// alert("Good Morning mam")
+
+//TASK3
+// var signal=prompt("Enter your color of road traffic signals")
+// if(signal=="red")
+// alert("must stop")
+// else if(signal=="Yellow")
+// alert("Ready to move")
+// else if(signal=="Green")
+// alert("Move now")
+
+//TASK4
+// var fuel=prompt("Enter remaining fuel in litres")
+// if(fuel<0.25)
+// alert("Please refill the fuel in your car")
+
+//TASK5
+// var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+//yeah it outputs the result as condition is true
+
+// var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+// //there is no output as b value is 82 when condition is checked
+
+// var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+
+// //condition 2 is true and condtion 4 is true
+
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+// //condition is true is cost equals
+
+// if (true){
+//     alert("True");
+//     }
+//     if (false){
+//     alert("False");}
+//     //output is true as condtion is has boolean value true
+    
+// if("car" < "cat"){
+//     alert("car is smaller than cat");
+//     }
+// //ouput is true because r is before than t in assci charaters
+
+
+//TASK6
+//var sub1=prompt("Enter subject name for 1")
+// var sub2=prompt("Enter subject name for 2")
+// var sub3=prompt("Enter subject name for 3")
+//  var total=100
+//  var obt1=prompt("Enter the obtained marks for the first subject")
+//  var obt2=prompt("Enter the obtained marks for the 2nd subject")
+//  var obt3=prompt("Enter the obtained marks for the 3rd subject")
+//  var per=(obt1+obt2+obt3)/total*3;
+//  if(per >= 80)
+//  document.write("A-one Grade") 
+//  else if(per >=70)  
+//  document.write("A")
+//  else if(per >=60)  
+//  document.write("B")
+//  document.write("YOu need to improve")
+//  else   
+//  document.write("Fail")
+
+
+// var no1= prompt("Guess the secret Number");
+// no2=2
+// if (no1 == no2 )  {
+//     msg=" Bingo Correct answer "
+// }
+// else if (no1==no2+1)
+// {
+//     msg="Close enough to the correct answer"
+// }
+// document.write(msg);
+
+
+
+// var n= prompt( "Enter a number");
+//  if  ( n%3==0 ) {
+//      msg="Given Number is divisible by 3 "
+//  }
+// else {
+//     msg="Given number is not divisible by 3"
+// }
+// document.write(msg); 
+
+
+// var n= prompt("Enter Even or odd number");
+// if ( n%2==0) {
+//     msg="Even Number"
+// }
+// else {
+// msg="Odd Number"
+// }
+// document.write(msg);
+
+// var t= prompt("Temperature");
+// if (t >40)  {
+//     msg=" it is too hot outside "
+// }
+// else if (t >30)  {
+//     msg=" The Weather Today is Nomral"
+// }
+// else if (t >20)  {
+//     msg=" Today's weather is cool"
+// }
+// else if (t >10)  {
+//     msg=" OMG! today's weather is so cool  "
+// }
+// else {
+//     msg="Wrong value"
+// }
+// document.write(msg);
